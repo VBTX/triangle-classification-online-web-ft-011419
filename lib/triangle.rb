12462fcd8bc826a2455e05f502b12b20c708fcfd
Victoria B <viktorija.bourassa@gmail.com>
2019-01-30 18:36:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,14 @@
 class Triangle
   attr_accessor :num1, :num2, :num3
   def initialize(num1, num2, num3)
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
+  end
+
+
+
+  
   end
 
   # write code here
